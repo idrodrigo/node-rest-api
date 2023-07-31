3e929e16a067bcfc4674516bd47ecf388841729b
Rho <idrodrigo@icloud.com>
2023-07-31 06:33:42 -0600

    rm log

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -38,7 +38,7 @@ export const AuthProvider = ({ children }) => {
 
       try {
         const res = await verifyTokenReq(cookies.token)
-        console.log(res)
+        // console.log(res)
         if (!res.data) return setIsAuthenticated(false)
         setIsAuthenticated(true)
         setUser(res.data)
